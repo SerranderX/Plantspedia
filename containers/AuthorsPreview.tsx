6c28ee1232027e3aa77ac7427704c10ec2b6e32e
SerranderX <felipe.diaz@xintec.cl>
2022-08-31 12:34:58 -0400

    :sparkles: [ADD] Titles in index page

--- containers/AuthorsPreview.tsx
+++ containers/AuthorsPreview.tsx
@@ -9,15 +9,20 @@ type AuthorsPreviewProps = {
 export const AuthorsPreview: React.FC<AuthorsPreviewProps> = ({authors}) => {
 
     return (
-        <div className="grid gap-x-1 grid-cols-4 pb-4 pt-2 border-solid border-t-4 border-purple-300 content-center">
-            {authors.map(author => 
-                <div key={author.id} className="w-20 xl:w-56 p-0 xl:p-3 justify-self-center mt-3 rounded-lg hover:bg-purple-400 opacity-90 hover:opacity-100 transition delay-50 duration-100 ease-in-out cursor-pointer">
-                    <ImageContentfull src={author.photo.url} layout="responsive" width={86} className="rounded-lg shadow-xl" aspectRatio='4:3' fit="fill" />
-                    <Typography variant="h4" component="h3" className="mt-2 mb-1">
-                        {author.fullName}
-                    </Typography>
-                </div>
-            )}
-        </div>
+        <>
+            <Typography variant="h2" component="h2" className="mt-2 mb-1">
+                Top authors
+            </Typography>
+            <div className="grid gap-x-1 grid-cols-4 pb-4 pt-2 border-solid border-t-4 border-purple-300 content-center">
+                {authors.map(author => 
+                    <div key={author.id} className="w-20 xl:w-56 p-0 xl:p-3 justify-self-center mt-3 rounded-lg hover:bg-purple-400 opacity-90 hover:opacity-100 transition delay-50 duration-100 ease-in-out cursor-pointer">
+                        <ImageContentfull src={author.photo.url} layout="responsive" width={86} className="rounded-lg shadow-xl" aspectRatio='4:3' fit="fill" />
+                        <Typography variant="h4" component="h3" className="mt-2 mb-1">
+                            {author.fullName}
+                        </Typography>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
--- containers/PostsPreview.tsx
+++ containers/PostsPreview.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { PlantCollection } from '@components/PlantCollection';
+import { Typography } from '@material-ui/core';
 
 type PostsPreviewProps = {
     plants: Plant[]
@@ -8,15 +9,20 @@ type PostsPreviewProps = {
 
 export const PostsPreview:React.FC<PostsPreviewProps> = ({ plants }) => {
     return (
-        <div className="border-solid border-t-4 border-purple-300 pt-4">
-            <PlantCollection plants={plants} variant={'square'} />
-            <div className="text-right px-2 h-full flex justify-end mb-4">
-                <Link href={`/posts`}>
-                    <a title={`See all Posts`} className="rounded-lg py-2 px-4 bg-purple-800 text-gray-100 text-2xl">
-                    See all Posts
-                    </a>
-                </Link>
+        <>
+            <Typography variant="h2" component="h2" className="mt-2 mb-1">
+                Recent Posts
+            </Typography>
+            <div className="border-solid border-t-4 border-purple-300 pt-4">
+                <PlantCollection plants={plants} variant={'square'} />
+                <div className="text-right px-2 h-full flex justify-end mb-4">
+                    <Link href={`/posts`}>
+                        <a title={`See all Posts`} className="rounded-lg py-2 px-4 bg-purple-800 text-gray-100 text-2xl">
+                        See all Posts
+                        </a>
+                    </Link>
+                </div>
             </div>
-        </div>
+        </>
     );
 };
