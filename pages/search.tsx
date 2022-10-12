b5f4deb645029743c766862a021fc39a16fddc72
SerranderX <felipediazalburquenque@gmail.com>
2022-10-12 19:03:28 -0300

    :sparkles: [ADD] Custom Debounce to search page

--- pages/search.tsx
+++ pages/search.tsx
@@ -27,13 +27,15 @@ export default function Search() {
   const [status, setStatus] = useState<QueryStatus>('idle')
   const [results, setResults] = useState<Plant[]>([])
 
+  const searchTerm = useDebounce(term, 900);
+
   const updateTerm: ChangeEventHandler<HTMLInputElement> = (event) =>
     setTerm(event.currentTarget.value)
 
   const emptyResults = status === 'success' && results.length === 0
 
   useEffect(() => {
-    if (term.trim().length < 1) {
+    if (searchTerm.trim().length < 1) {
       setStatus('idle')
       setResults([])
       return
@@ -41,15 +43,14 @@ export default function Search() {
 
     setStatus('loading')
 
-    // Pagination not supported... yet
     searchPlants({
-      term,
+      term: searchTerm,
       limit: 10,
     }).then((data) => {
       setResults(data)
       setStatus('success')
     })
-  }, [term])
+  }, [searchTerm])
 
   return (
     <Layout>
@@ -83,4 +84,22 @@ export default function Search() {
       </main>
     </Layout>
   )
+}
+
+function useDebounce<T>(value: T, wait:number = 0) {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  console.log(value);
+  console.log(wait);
+
+  useEffect(() => {
+    const timeoutId = window.setTimeout(() => {
+      setDebouncedValue(value)
+    }, wait);
+
+    return () => {
+      window.clearTimeout(timeoutId)
+    }
+  }, [value])
+
+  return debouncedValue
 }
